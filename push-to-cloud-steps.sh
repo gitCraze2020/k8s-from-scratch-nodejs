c797997ec808e7cc88248e8826b14887232f38cb
gitCraze2020 <jurgenstoop@yahoo.com>
2020-03-04 17:35:36 -0500

    my comment auto

---
+++ push-to-cloud-steps.sh
@@ -0,0 +1,17 @@
+﻿# build docker image from current folder
+docker image build -t dockercraze/k8s-from-scratch-nodejs .
+
+# test as needed
+
+# push image to dockerhub
+docker push dockercraze/k8s-from-scratch-nodejs
+
+# add changes to staging
+git add .
+# commit locally
+git commit -m "my comment auto"
+# push changes to github repository
+git push -u origin master
+
+# deploy in kubernetes
+kubectl apply -f deployment-k8s-from-scratch-nodejs.yml
